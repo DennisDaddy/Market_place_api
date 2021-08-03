df5b79de19a3a04e16dddb169da9510192dce767
DennisDaddy <softteez95@gmail.com>
2021-08-03 08:28:55 +0300

    Update product

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -12,7 +12,22 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(self.response.body)
     assert_equal @product.title, json_response['title']
   end
-  
+
+  test 'should update product' do
+    patch api_v1_product_url(@product), 
+          params: { product: { title: @product.title } }, 
+          headers: { Authorization: JsonWebToken.encode(user_id:
+   @product.user_id) }, as: :json
+    assert_response :success
+  end
+
+  test 'should forbid update product' do
+    patch api_v1_product_url(@product),
+    params: { product: { title: @product.title } },
+    headers: { Authorization: JsonWebToken.encode(user_id:
+    users(:two).id) }, as: :json
+    assert_response :forbidden
+  end
   
 
 end
